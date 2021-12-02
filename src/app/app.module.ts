073446ca93aedbcd7bb0c6ef2012fcfa8b66189c
Julia Gonzalez <vanesa_garro@hotmail.com>
2021-12-02 17:58:45 -0300

    validaciones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 
 import { FormRegistroComponent } from './components/form-registro/form-registro.component';
 import { FormLoginComponent } from './components/form-login/form-login.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { FormLoginComponent } from './components/form-login/form-login.component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/form-registro/form-registro.component.ts
+++ src/app/components/form-registro/form-registro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-registro',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormRegistroComponent implements OnInit {
 
+
+
+  registroForm = new FormGroup ({
+    nombre: new FormControl ('', Validators.required),
+    email: new FormControl ('', Validators.email),
+    password: new FormControl ('', [Validators.required, Validators.minLength(8)])
+  });
+
+  nombreControl = this.registroForm.controls['nombre'];
+  emailControl = this.registroForm.controls['email'];
+  passwordControl = this.registroForm.controls['password'];
+
+
   constructor() { }
 
   ngOnInit(): void {
+
+    //this.registroForm.valueChanges.subscribe(values => console.log('values change', values));
+    this.registroForm.controls['nombre'].valueChanges.subscribe(values => console.log('values change', values))
+  }
+
+  registrar() {
+    console.log(this.registroForm.value);
   }
 
 }
